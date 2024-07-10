abd3e429a3482d66713ce886dbdaa752d009af13
irwinpannu <101152449+irwinpannu@users.noreply.github.com>
2024-07-10 10:36:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var countdownElement = document.getElementById("countdown");
   
     function updateCountdown() {
-      var targetDate = new Date("June 02, 2024 00:00:00").getTime();
+      var targetDate = new Date("September 10, 2024 00:00:00").getTime();
       var now = new Date().getTime();
       var remainingTime = targetDate - now;
   
